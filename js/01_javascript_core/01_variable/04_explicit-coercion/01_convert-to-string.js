d6811e316d455d16adbc5f2bf99b066249381973
haenin <gpdnjsqhdld@naver.com>
2025-10-15 12:50:35 +0900

    js study

--- js/01_javascript_core/01_variable/04_explicit-coercion/01_convert-to-string.js
+++ js/01_javascript_core/01_variable/04_explicit-coercion/01_convert-to-string.js
@@ -1,5 +1,5 @@
 /* 1. String 생성자 함수를 new 연산자 없이 호출하는 방법 */
-console.log(String(new 10));
+console.log(String(10));
 console.log(String(NaN));
 console.log(String(Infinity));
 console.log(String(true));
