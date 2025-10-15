e3fb3093c1161fdfde9c60e839df64976489f5f7
haenin <gpdnjsqhdld@naver.com>
2025-10-15 12:49:35 +0900

    js study

---
+++ js/01_javascript_core/03_object-literal/03_ES6/02_method-shorthand.js
@@ -0,0 +1,17 @@
+/* 메서드 단축 */
+
+var pika = {
+    name: '피카츄',
+    attack: function(skill) {
+        console.log(`${skill} 시전`);
+    }
+};
+pika.attack('백만볼트');
+
+var pika2 = {
+    name: '피카츄',
+    attack(skill) {
+        console.log(`${skill} 시전`); // 콜론과 function이라는 키워드를 안쓰고 메서드를 작성할 수 있다.
+    }
+};
+pika2.attack('백만볼트');
