dd08f386004e679e9b86fcb45e347d6583ca3d17
haenin <gpdnjsqhdld@naver.com>
2025-10-23 15:44:38 +0900

    vue study

---
+++ vue/08_props/src/components/01_propsBasic/ReadProps.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ props.readValue }}</h1>
+    <label> 자식의 readValue:</label>
+    <input type="text" @change="test($event)" v-model="props.readValue">
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        readValue: String
+    })
+</script>
+
+<style scoped>
+
+</style>
