042f05467ab0e0506fa3eb74bc65422dc252a786
haenin <gpdnjsqhdld@naver.com>
2025-10-23 15:44:40 +0900

    vue study

---
+++ vue/08_props/src/components/02_propsDriling/DrilingChild.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <DrilingGrandChild></DrilingGrandChild>
+    </div>
+</template>
+
+<script setup>
+    import DrilingGrandChild from './DrilingGrandChild.vue';
+</script>
+
+<style scoped>
+
+</style>
