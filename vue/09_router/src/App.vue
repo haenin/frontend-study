9fa2039feab3d42a81d6b2ff370cbbfd0ff4f4e1
haenin <gpdnjsqhdld@naver.com>
2025-10-23 15:45:23 +0900

    vue study

---
+++ vue/09_router/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+  import {RouterLink, RouterView} from 'vue-router';
+<template>
+  <header>
+    <h2>basic router</h2>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/pathvariable/1">PathVariable</RouterLink>
+      <RouterLink to="/querystring?name=홍길동&age=20">QueryString</RouterLink>
+    </nav>
+  </header>
+  <main>
+    <RouterView/>
+  </main>
+</template>
+
+<style scoped></style>
