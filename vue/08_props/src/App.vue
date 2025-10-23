e201872c091600cc79357605e18890435d5f1a4d
haenin <gpdnjsqhdld@naver.com>
2025-10-23 14:00:51 +0900

    vue study

---
+++ vue/08_props/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <PropsParent/>
+  </header>
+  <main>
+
+  </main>
+</template>
+
+<script setup>
+  import PropsParent from './components/01_propsBasic/PropsParent.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
