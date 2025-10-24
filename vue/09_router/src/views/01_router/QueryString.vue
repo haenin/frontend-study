58338e54051f4c86d3565d8503a5fe43e61fbe4b
haenin <gpdnjsqhdld@naver.com>
2025-10-24 10:39:03 +0900

    vue study

--- vue/09_router/src/views/01_router/QueryString.vue
+++ vue/09_router/src/views/01_router/QueryString.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
         <h1>QueryString</h1>
+        <h2>넘어온 query string 값: {{ currentRoute.query }}</h2>
+        <h2>이름: {{ name }}</h2>
+        <h2>나이: {{ age }}</h2>
     </div>
 </template>
 
 <script setup>
+    import {useRoute} from 'vue-router';
 
+    const currentRoute = useRoute();
+    const name = currentRoute.query.name;
+    const age = currentRoute.query.age;
 </script>
 
 <style scoped>
