97231c3c7bb2fcf3326065cf2bd9dcd08e4a5d72
haenin <gpdnjsqhdld@naver.com>
2025-10-24 10:18:04 +0900

    vue study

---
+++ vue/09_router/src/views/02_nestedRouter/RootRouter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>RootRouter</h1>
+        <RouterView/>
+    </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+
+
+</script>
+
+<style scoped>
+
+</style>
