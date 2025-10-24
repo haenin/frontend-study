ae98c871462cea45353d454958f18b19a4ba0bc0
haenin <gpdnjsqhdld@naver.com>
2025-10-24 10:22:34 +0900

    vue study

--- vue/09_router/src/main.js
+++ vue/09_router/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/01_router.js';
+// import router from './router/01_router.js';
+import router2 from './router/02_nestedRouter';
 
 /* npm install vue-router@next로 dependency 추가 */
 // createApp(App).user(router).mount('#app')
 // 위에가 너무길어지면 밑에처럼 ..추가
 const app = createApp(App);
-app.use(router);
+// app.use(router);
+app.use(router2);
 app.mount('#app');
