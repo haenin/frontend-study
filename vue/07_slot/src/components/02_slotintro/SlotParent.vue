23480f18564d668f7a10ca5ec8517efd63821233
haenin <gpdnjsqhdld@naver.com>
2025-10-25 23:52:47 +0900

    vue study

---
+++ vue/07_slot/src/components/02_slotintro/SlotParent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="parentContainer">
+        <Child>
+            {{  slotData }}
+        </Child>
+    </div>
+</template>
+
+<script setup>
+    import Child from './SoltChild.vue';
+    import {ref} from 'vue';
+
+    /*
+        slot은 일반적으로 태그 형태로 전달된다.
+        (일반적으로 상태값, 데이터를 활용한 태그는 뒤에 배울 props을 활용하는 편이다.)
+    */
+    const slotData = ref('Hello data');
+</script>
+
+<style scoped>
+    .parentContainer {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        border: 1px solid pink;
+    }
+</style>
